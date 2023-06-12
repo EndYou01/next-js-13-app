92b2eaf913203eab753dc100180dd8997f97ccf3
EndYou01 <jorgesenjudo@gmail.com>
2023-06-12 09:41:19 -0400

    Implementacion de Next/Image

--- app/(with-banner)/posts/[id]/comments/page.jsx
+++ app/(with-banner)/posts/[id]/comments/page.jsx
@@ -1,16 +1,18 @@
 
+import Image from 'next/image'
+
 const fetchComments = async (id) => {
 
     await new Promise(resolve => setTimeout(resolve, 3000))
 
-    throw new Error('Error al cargar los comentarios')
+    // throw new Error('Error al cargar los comentarios')
 
-    // return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
-    //     next: {
-    //         revalidate: 60
-    //     }
-    // })
-    //     .then(res => res.json())
+    return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
+        next: {
+            revalidate: 60
+        }
+    })
+        .then(res => res.json())
 }
 
 export default async function Post({ params }) {
@@ -20,9 +22,10 @@ export default async function Post({ params }) {
     const comments = await fetchComments(id)
 
     return (
-        <ul style={{ background: '#444', fontSize: '12px' }}>
+        <ul style={{ background: '#444', fontSize: '12px', padding: '20px', marginLeft: '10px' }}>
             {comments.map(comment => (
                 <li key={comment.id}>
+                    <Image width='50' height='50' src={`https://avatars.dicebear.com/api/pixel-art-neutral/${comment.email}.svg`} alt={comment.name}/>
                     <h4>{comment.name}</h4>
                     <small>{comment.body}</small>
                 </li>
